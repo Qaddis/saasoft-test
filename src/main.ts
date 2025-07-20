0a7465178a8201fd3a4a5208e21f3fb9ff402713
Qaddis <svyatoslavb1107@gmail.com>
2025-07-20 15:42:09 +0300

    Styling configured

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { createApp } from "vue"
 
+import Aura from "@primeuix/themes/aura"
+import PrimeVue from "primevue/config"
+
 import App from "./App.vue"
+import "./css/globals.css"
 
 const app = createApp(App)
+
+// Подключение PrimeVue в проект
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura
+	}
+})
 
 app.mount("#app")
